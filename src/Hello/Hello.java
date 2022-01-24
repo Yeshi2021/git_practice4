9519c1b5c41aa84fa865b766e9ce64bc8e3ffcb5
veronica <veronicabh21@hotmail.com>
2022-01-24 10:39:01 -0500

    initial work is done

--- src/Hello/Hello.java
+++ src/Hello/Hello.java
@@ -1,4 +1,13 @@
 package Hello;
 
 public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello World");
+        System.out.println("B25 Learning git and gitHub");
+
+
+
+    }
 }
