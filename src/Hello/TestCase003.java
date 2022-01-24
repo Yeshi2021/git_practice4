bcdda4ecfcfd3c0eba70a5a6ea0cdf2455e0b63a
veronica <veronicabh21@hotmail.com>
2022-01-24 12:48:11 -0500

    Creating conflict in TestCase003 in master

--- src/Hello/TestCase003.java
+++ src/Hello/TestCase003.java
@@ -2,14 +2,14 @@ package Hello;
 
 public class TestCase003 {
 
-<<<<<<< HEAD
+
     // this is unknow
     // that exist
     // now git not know
     // when we merge
-=======
+
     // awesome code
 
     // code finished in feauture branch
->>>>>>> tc003
+
 }
